493f97b76a281f8c4240b37ca19434f803c3324a
stephen bennett <s.bennett87@gmail.com>
2017-05-29 11:44:57 -0400

    'more updates'

--- src/app/product.ts
+++ src/app/product.ts
@@ -3,28 +3,21 @@ import { productModel } from './product.model';
 // const t = JSON.parse(window.localStorage.getItem('model'));
 const t = productModel;
 
-const pushToStorage = (model) => {
-    window.localStorage.setItem('model',JSON.stringify(model));
-}
-
 export const product = ( state = t, action ) => {
-    
-    switch(action.type) {
-
+        switch(action.type) {
+        
         case "UPDATE_FIGURE":
-            var obj = Object.assign({},state);
-            obj[action.payload[0]] = action.payload[1];
-            pushToStorage(obj);
-            return obj
+            console.time();
+            return {
+                ...state,
+                [action.payload[0]]: action.payload[1]
+            };
 
         case "UPDATE_FACE":
-            var obj = Object.assign({},state);
-            obj.face[action.payload[0]] = action.payload[1];
-            pushToStorage(obj);
-            return obj
+                let _state = { ...state.face, [action.payload[0]]: action.payload[1]};
+                return { ...state, ['face']: _state  };
         
         default: 
             return state;
     }
-    
 }
