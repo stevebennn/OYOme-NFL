d82b3a98e700296649b328883453ea1d0d6b1d3f
stephen bennett <s.bennett87@gmail.com>
2017-05-29 19:04:36 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {Store,provideStore, StoreModule} from '@ngrx/store';
 
-import {Store,provideStore} from '@ngrx/store';
-
+// top level app component
 import { AppComponent } from './app.component';
 
-import { productModel } from './product.model';
-import { StepsService } from './steps.service';
+// custom application components
 import { FigureComponent } from './figure/figure.component';
 import { FigureFeatureViewComponent } from './figure-feature-view/figure-feature-view.component';
 
+// service to provide steps and available options
+import { StepsService } from './steps.service';
+
+// data model for the product
+import { productModel } from './product.model';
+
+// product reducer
 import { product } from './product';
 
 @NgModule({
--- src/app/product.ts
+++ src/app/product.ts
@@ -1,22 +1,23 @@
+import { Store, Action, ActionReducer, combineReducers } from '@ngrx/store';
 import { productModel } from './product.model';
 
-// const t = JSON.parse(window.localStorage.getItem('model'));
-const t = productModel;
+const _productModel = productModel;
 
-export const product = ( state = t, action ) => {
+export const product = ( state = _productModel, action ) => {
         switch(action.type) {
-        
+
+        // updates figure features of the product state
         case "UPDATE_FIGURE":
             return {
                 ...state,
                 [action.payload[0]]: action.payload[1]
             };
 
+        // updates facial features of the product state    
         case "UPDATE_FACE":
             let _state = { ...state.face, [action.payload[0]]: action.payload[1]};
             return { ...state, ['face']: _state  };
         
-
         case "RESET":
             return productModel
 
